9a788d6284972b86ba4db74abb74590d671f75a1
Victor Marcolino <vctor.marcolino@gmail.com>
2024-05-29 21:28:36 -0300

    Trabalho 2905
    Trabalho

---
+++ Cliente.py
@@ -0,0 +1,18 @@
+import socket
+
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+cliente.connect(('localhost', 7777))
+print('Conectado!\n')
+
+namefile = str(input('Arquivo>'))
+
+cliente.send(namefile.encode())
+
+with open(namefile, 'wb') as file:
+    while 1:
+        data = cliente.recv(1000000)
+        if not data:
+            break
+        file.write(data)
+
+    print(f'{namefile} recebido\n')
